d9eee91582ac4f4a59cd2626943f5b61ccde5a3b
jwstes <jwstes@gmail.com>
2024-01-16 04:06:02 +0800

    Added Main Menu, fixed splash screen and fixed slag rendering

--- core/src/com/mygdx/game/AmpEngine.java
+++ core/src/com/mygdx/game/AmpEngine.java
@@ -14,17 +14,31 @@ public class AmpEngine extends ApplicationAdapter{
 	private SceneManager sceneManager;
 	private long startTime;
 	
+	private int selectedMenuIndex;
+	
 	@Override
 	public void create() {
+		sceneManager = new SceneManager();
+		sceneManager.setDeveloperLogo("badlogic.jpg");
+		
 		startTime = TimeUtils.nanoTime();
+		selectedMenuIndex = 2;
 	}
 	
 	
 	@Override
 	public void render() {
-		sceneManager = new SceneManager();
-		sceneManager.setDeveloperLogo("badlogic.jpg");
-		sceneManager.displaySplashScreen(true, 2, startTime);
+		boolean splashScreen = sceneManager.displaySplashScreen(true, 2, startTime);
+		if(splashScreen) {
+			String[] menuItems = {"Exit", "Options", "Start Game"};
+			Runnable[] actions = {
+	                () -> System.out.println("Exit selected"),
+	                () -> System.out.println("Options selected"),
+	                () -> System.out.println("Start Game selected")
+	        };
+			selectedMenuIndex = sceneManager.displayMenu(menuItems, actions, selectedMenuIndex);
+		}
+		
 	}
 	
 	public AmpEngine() {
--- core/src/com/mygdx/game/SceneManager.java
+++ core/src/com/mygdx/game/SceneManager.java
@@ -10,7 +10,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.Timer;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.Input;
 
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
+import com.badlogic.gdx.graphics.Color;
 
 
 public class SceneManager{
@@ -31,10 +38,10 @@ public class SceneManager{
 	}
 	
 	public void setDeveloperLogo(String assetName) {
-		developerLogo = entityManager.loadTexture(assetName);
+		developerLogo = entityManager.setTexture(assetName);
 	}
 	
-	public void displaySplashScreen(boolean timerMode, int seconds, long startTime) {
+	public boolean displaySplashScreen(boolean timerMode, int seconds, long startTime) {
 		clearScreen();
 		
 		SpriteBatch spriteBatch = new SpriteBatch();
@@ -49,12 +56,79 @@ public class SceneManager{
 		
 		if(timerMode == true) {
 			float elapsedTime = (TimeUtils.nanoTime() - startTime) / 1000000000.0f;
-			if(elapsedTime > seconds) {
-				clearScreen();
-			}
+			return elapsedTime > seconds;
 		}
-		
-		
+		return false;
+	}
+	
+	public int displayMenu(String[] menuItemText, Runnable[] actions, int selectedMenuItemIndex) {
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+	    SpriteBatch spriteBatch = new SpriteBatch();
+	    ShapeRenderer shapeRenderer = new ShapeRenderer();
+	    BitmapFont font = new BitmapFont();
+	    OrthographicCamera camera = new OrthographicCamera();
+	    camera.setToOrtho(false);
+
+	    shapeRenderer.setProjectionMatrix(camera.combined);
+	    spriteBatch.setProjectionMatrix(camera.combined);
+
+	    float menuItemWidth = 300;
+	    float menuItemHeight = 100;
+	    float menuItemSpacing = 20;
+
+	    float totalHeight = menuItemText.length * menuItemHeight + (menuItemText.length - 1) * menuItemSpacing;
+
+	    float startY = (Gdx.graphics.getHeight() - totalHeight) / 2;
+	    
+	    
+	    if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
+            selectedMenuItemIndex = Math.max(0, selectedMenuItemIndex - 1);
+        }
+	    else if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+            selectedMenuItemIndex = Math.min(menuItemText.length - 1, selectedMenuItemIndex + 1);
+        }
+	    else if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+            if (selectedMenuItemIndex >= 0 && selectedMenuItemIndex < actions.length) {
+                actions[selectedMenuItemIndex].run();
+            }
+        }
+	    
+	    
+	    for (int i = 0; i < menuItemText.length; i++) {
+	        float rectangleX = (Gdx.graphics.getWidth() - menuItemWidth) / 2;
+	        float rectangleY = startY + i * (menuItemHeight + menuItemSpacing);
+
+	        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+	        
+	        
+	        if (i == selectedMenuItemIndex) {
+                shapeRenderer.setColor(Color.RED);
+            } else {
+                shapeRenderer.setColor(1, 1, 1, 1);
+            }
+	        
+	        shapeRenderer.rect(rectangleX, rectangleY, menuItemWidth, menuItemHeight);
+	        shapeRenderer.end();
+
+	        spriteBatch.begin();
+
+	        font.setColor(0, 0, 0, 1);
+
+	        String currentMenuItemText = menuItemText[i];
+
+	        GlyphLayout glyphLayout = new GlyphLayout();
+	        glyphLayout.setText(font, currentMenuItemText);
+
+	        float textX = rectangleX + (menuItemWidth - glyphLayout.width) / 2;
+	        float textY = rectangleY + (menuItemHeight + glyphLayout.height) / 2;
+
+	        font.draw(spriteBatch, currentMenuItemText, textX, textY);
+
+	        spriteBatch.end();
+	    }
+	    return selectedMenuItemIndex;
 	}
 	
 	public SceneManager() {
