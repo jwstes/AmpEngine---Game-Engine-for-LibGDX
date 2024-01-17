ca0d4828987ced2562ac9cac3fd0766586be0198
Jonathan <tokjonathan@gmail.com>
2024-01-17 20:30:30 +0800

    Deleted MyGDXGame

--- desktop/src/com/mygdx/game/Configuration.java
+++ desktop/src/com/mygdx/game/Configuration.java
@@ -1,9 +1,5 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
-import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
-
 public class Configuration {
 	
 	public int wHeight = 720;
