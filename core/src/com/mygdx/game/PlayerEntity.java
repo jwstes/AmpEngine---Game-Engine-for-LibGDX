f0d357b46ab1f2f3100bf786a0a18d93333e0baf
LGD458 <limguoodongg@hotmail.com>
2024-01-21 22:23:23 +0800

    Added the control up,down,left,right. A draft

--- core/src/com/mygdx/game/PlayerEntity.java
+++ core/src/com/mygdx/game/PlayerEntity.java
@@ -12,15 +12,42 @@ import com.badlogic.gdx.utils.TimeUtils;
 import org.w3c.dom.Text;
 import com.badlogic.gdx.math.Rectangle;
 import org.w3c.dom.css.Rect;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 
 
 /* This Class handles the Player Entity (Player's Character/Sprite) only.*/
 /* This record character's health, positioning, image texture and hitbox(rectangle) */
 /* Possible implementations to split into smaller classes possible. Possible addition such as guns, etc to be decided)*/
-public class PlayerEntity extends EntityManager{
-
-
-
-
+public abstract class PlayerEntity extends EntityManager implements InputProcessor{
+	public PlayerEntity(int initialHealth) {
+		super(initialHealth); //mean to call the constructor of the parent class
+	}
+	
+	@Override
+	public boolean keyDown(int keyPressed) {
+		return false;
+	}
+	
+	@Override
+	public boolean keyUp(int keyPressed) {
+		handleInput(keyPressed);
+		return false;
+	}
+	private void handleInput(int keyPressed) {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            setPosX(this.getPosX() - PLAYER_SPEED * Gdx.graphics.getDeltaTime());
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            setPosX(this.getPosX() + PLAYER_SPEED * Gdx.graphics.getDeltaTime());
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            setPosY(this.getPosY() + PLAYER_SPEED * Gdx.graphics.getDeltaTime());
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            setPosY(this.getPosY() - PLAYER_SPEED * Gdx.graphics.getDeltaTime());
+        }
+    
+	
 
 }
