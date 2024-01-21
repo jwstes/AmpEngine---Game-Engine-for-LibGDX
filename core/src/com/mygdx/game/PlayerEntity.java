e687f4b72c419f5553eee757993c34bb98a3ad6e
LGD458 <limguoodongg@hotmail.com>
2024-01-21 22:30:02 +0800

    Added Control Key Left,Right,Up,Down

--- core/src/com/mygdx/game/PlayerEntity.java
+++ core/src/com/mygdx/game/PlayerEntity.java
@@ -48,6 +48,5 @@ public abstract class PlayerEntity extends EntityManager implements InputProcess
             setPosY(this.getPosY() - PLAYER_SPEED * Gdx.graphics.getDeltaTime());
         }
     
-	
-
+	}
 }
